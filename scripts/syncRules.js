fe8cbe18680231b4b8dd3475bcdb495dbfd0e5d8
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2025-04-05 02:04:48 +0900

    Create script for sync agent rules

--- scripts/syncRules.js
+++ scripts/syncRules.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const rulesDir = path.join(__dirname, '../.cursor/rules');
-const outputFile = path.join(__dirname, '../.windsurfrules');
+const rulesDir = path.join(__dirname, "../.cursor/rules");
+const outputFile = path.join(__dirname, "../.windsurfrules");
 
 fs.readdir(rulesDir, (err, files) => {
   if (err) {
-    console.error('Error reading rules directory:', err);
+    console.error("Error reading rules directory:", err);
     return;
   }
 
-  let combinedContent = '';
+  let combinedContent = "";
 
   files.forEach((file) => {
     const filePath = path.join(rulesDir, file);
-    const content = fs.readFileSync(filePath, 'utf8');
-    combinedContent += content + '\n';
+    const content = fs.readFileSync(filePath, "utf8");
+    combinedContent += content + "\n";
   });
 
-  fs.writeFileSync(outputFile, combinedContent, 'utf8');
-  console.log('Rules have been synchronized into .windsurfrules');
+  fs.writeFileSync(outputFile, combinedContent, "utf8");
+  console.log("Rules have been synchronized into .windsurfrules");
 });
