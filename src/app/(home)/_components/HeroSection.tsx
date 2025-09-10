de664bfbea2337608aa7e2b1c168f41e5f76d99e
syosyo10337 <syosyo.ds@gmail.com>
2025-09-11 01:57:30 +0900

    feat:style調整

--- src/app/(home)/_components/HeroSection.tsx
+++ src/app/(home)/_components/HeroSection.tsx
@@ -1,22 +1,21 @@
 import HeroSectionTopFrame from "@/assets/HeroSectionTopFrame.optimized.svg";
 import HeroSecBottomFrame from "@/assets/HeroSecBottomFrame.optimized.svg";
 import Image from "next/image";
+
 export default function HeroSection() {
   return (
-    <section className="hero-section">
-      <HeroSectionTopFrame className="w-full pointer-events-none" />
-        <Image
-          src="/images/top_demo.jpg"
-          alt="Hero Section Background"
-          width={1000}
-          height={1000}
-          className="object-cover"
-        />
-      <h2 className="text-white font-bold leading-tight hero-title">
-        好きに生きて、一緒に生きる
-      </h2>
-      <p className="text-white/80 mt-2 hero-subtitle">ABOUT SOY-POY</p>
-      <HeroSecBottomFrame className="w-full pointer-events-none" />
+    <section className="relative w-full h-screen overflow-hidden hero-section">
+      {/* 背景画像 */}
+      <Image
+        src="/images/top_demo.jpg"
+        alt="Hero Section Background"
+        fill
+        className="object-cover z-0"
+        priority
+      />
+      
+      <HeroSectionTopFrame className="absolute top-0 left-0 w-full pointer-events-none z-10" />
+      <HeroSecBottomFrame className="absolute bottom-0 left-0 w-full pointer-events-none z-10" />
     </section>
   );
 }
