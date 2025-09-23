b62db42b55cfd1e8fbbe6ba9cefdde1afb54786e
syosyo10337 <syosyo.ds@gmail.com>
2025-09-24 02:41:34 +0900

    feat:filmrollを追加

---
+++ src/app/(home)/_components/FudaFilmRollBg.tsx
@@ -0,0 +1,17 @@
+// FIME: 実質のスタイルがwidthではないところで実装されている。
+export default function FudaFilmRollBg() {
+  return (
+    <div className="fixed inset-0 w-full h-screen pointer-events-none overflow-hidden">
+      <div className="flex justify-between items-stretch h-full">
+        {/* 左側のフィルムロール */}
+        <div className="flex-shrink-0 h-screen w-8 md:w-14 bg-filmroll-left film-roll-flow" />
+
+        {/* 中央の空白スペース */}
+        <div className="flex-grow" />
+
+        {/* 右側のフィルムロール */}
+        <div className="flex-shrink-0 h-screen w-8 md:w-14 bg-filmroll-right film-roll-flow" />
+      </div>
+    </div>
+  );
+}
--- src/app/(home)/_components/HeroSection/index.tsx
+++ src/app/(home)/_components/HeroSection/index.tsx
@@ -13,7 +13,7 @@ export default function HeroSection() {
       <FudaOverLay className="hidden md:block absolute inset-0 w-full h-full pointer-events-none z-30 overflow-hidden" />
       <HeroSectionTopFrame className="absolute top-1 left-0 w-full pointer-events-none z-10" />
       <HeroSecBottomFrame
-        className="absolute -bottom-50 md:bottom-2 z-10 w-full px-4
+        className="absolute -bottom-50 md:bottom-2 z-10 w-full px-8
         md:left-1/2 md:-translate-x-1/2 md:w-7/10 md:px-0"
       />
     </section>
