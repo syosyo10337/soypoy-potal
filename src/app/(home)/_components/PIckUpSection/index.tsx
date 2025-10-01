3d1f2d93876016d249ff32b92fb275c15acce741
syosyo10337 <syosyo.ds@gmail.com>
2025-10-02 01:57:30 +0900

    z-indexを調整

--- src/app/(home)/_components/PIckUpSection/index.tsx
+++ src/app/(home)/_components/PIckUpSection/index.tsx
@@ -2,10 +2,11 @@ import { PickUpMarquee, PickUpMarqueeReverse } from "./PickUpMarquee";
 
 export default function PickUpSection() {
   return (
-    <section className="relative bg-soypoy-secondary text-white z-20 min-h-screen">
+    //NOTE:  relativeとbgでfudaより前に配置する。
+    <section className="relative bg-soypoy-main text-white">
       <PickUpMarquee />
       <div className="container mx-auto px-6 h-full flex flex-col justify-center items-center text-center">
-        <h2 className="text-4xl md:text-6xl font-bold mb-6">Pick Up WIP</h2>
+        <h2 className="text-4xl md:text-6xl font-bold mb-6 text-soypoy-secondary">Pick Up Section</h2>
       </div>
       <PickUpMarqueeReverse />
     </section>
