9a1f83e8d67e261f47013e46a46ccf63fd25506d
syosyo10337 <syosyo.ds@gmail.com>
2025-10-11 01:57:14 +0900

    feat:Headerの調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import {
   notoSansJP,
 } from "@/assets/fonts";
 import Footer from "@/components/Footer";
-import Header from "@/components/Header/Header";
+import Header from "@/components/Header";
 import { cn } from "@/utils/cn";
 
 export const metadata: Metadata = {
