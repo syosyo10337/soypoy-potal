56d0e7dc96ab5df987b5618b827b51aa70943694
syosyo10337 <syosyo.ds@gmail.com>
2025-10-11 03:03:14 +0900

    feat:FudaOverLayを再実装 svg -> png

--- src/app/(home)/_components/HeroSection/ArchDecoration/FudaOverLay/index.tsx
+++ src/app/(home)/_components/HeroSection/ArchDecoration/FudaOverLay/index.tsx
@@ -1,17 +1,17 @@
 import { cn } from "@/utils/cn";
 import {
-  ComedyFuda,
-  CraftGinSvg,
-  DanceFuda,
-  ImproFuda,
-  MovieFuda,
-  MusicFuda,
-  PhotoFuda,
-  SessionFuda,
-  ShimokitazawaStationSvg,
-  StatueOfLibertySvg,
-  TheaterFuda,
+  ComedyFudaImage,
+  DanceFudaImage,
+  ImproFudaImage,
+  MovieFudaImage,
+  MusicFudaImage,
+  PhotoFudaImage,
+  SessionFudaImage,
+  StationAndGinImage,
+  StatueOfLibertyImage,
+  TheaterFudaImage,
 } from "./assets";
+import FudaImage from "./FudaImage";
 
 interface FudaOverLayProps {
   className?: string;
@@ -22,44 +22,79 @@ export default function FudaOverLay({ className }: FudaOverLayProps) {
   return (
     <div className={cn(className)}>
       {/* 左上エリア */}
-      <div className="absolute top-30 left-80 transform -rotate-18">
-        <MusicFuda className="w-10 h-auto" />
+      <div
+        className="absolute top-[7%] left-[14%] will-change-transform animate-float-1"
+        style={{ "--rotate": "-18deg" } as React.CSSProperties}
+      >
+        <FudaImage src={MusicFudaImage} alt="Music" className="w-[14%]" />
       </div>
-      <div className="absolute top-50 left-40 transform rotate-8">
-        <SessionFuda className="w-10 h-auto" />
+      <div
+        className="absolute top-[12%] left-[7%] will-change-transform animate-float-2"
+        style={{ "--rotate": "8deg" } as React.CSSProperties}
+      >
+        <FudaImage src={SessionFudaImage} alt="Session" className="w-[14%]" />
       </div>
-      <div className="absolute top-80 transform -rotate-10">
-        <TheaterFuda className="w-10 h-auto" />
+      <div
+        className="absolute top-[19%] left-0 will-change-transform animate-float-3"
+        style={{ "--rotate": "-10deg" } as React.CSSProperties}
+      >
+        <FudaImage src={TheaterFudaImage} alt="Theater" className="w-[14%]" />
       </div>
 
       {/* 右上エリア */}
-      <div className="absolute top-30 right-90 transform rotate-12">
-        <ImproFuda className="w-10 h-auto" />
+      <div
+        className="absolute top-[7%] right-[16%] will-change-transform animate-float-4"
+        style={{ "--rotate": "12deg" } as React.CSSProperties}
+      >
+        <FudaImage src={ImproFudaImage} alt="Impro" className="w-[14%]" />
       </div>
-      <div className="absolute top-38 right-6 transform rotate-14">
-        <DanceFuda className="w-10 h-auto" />
+      <div
+        className="absolute top-[9%] right-[1%] will-change-transform animate-float-5"
+        style={{ "--rotate": "14deg" } as React.CSSProperties}
+      >
+        <FudaImage src={DanceFudaImage} alt="Dance" className="w-[14%]" />
       </div>
-      <div className="absolute top-46 right-46 transform -rotate-8">
-        <ComedyFuda className="w-10 h-auto" />
+      <div
+        className="absolute top-[11%] right-[8%] will-change-transform animate-float-6"
+        style={{ "--rotate": "-8deg" } as React.CSSProperties}
+      >
+        <FudaImage src={ComedyFudaImage} alt="Comedy" className="w-[14%]" />
       </div>
-      <div className="absolute top-80 right-4 transform rotate-10">
-        <MovieFuda className="w-10 h-auto" />
+      <div
+        className="absolute top-[19%] right-[0.7%] will-change-transform animate-float-1"
+        style={{ "--rotate": "10deg" } as React.CSSProperties}
+      >
+        <FudaImage src={MovieFudaImage} alt="Movie" className="w-[14%]" />
       </div>
 
       {/* 左下エリア */}
-      <div className="absolute top-1/2 -left-20 transform -rotate-12">
-        <ShimokitazawaStationSvg className="w-80 h-auto" />
-      </div>
-      <div className="absolute bottom-67 left-26 transform rotate-16">
-        <CraftGinSvg className="w-16 h-auto" />
+      <div
+        className="absolute top-1/2 -left-[3.5%] will-change-transform animate-float-2"
+        style={{ "--rotate": "-12deg" } as React.CSSProperties}
+      >
+        <FudaImage
+          src={StationAndGinImage}
+          alt="Shimokitazawa Station"
+          className="w-[70%]"
+        />
       </div>
 
       {/* 右下エリア */}
-      <div className="absolute bottom-84 right-0 transform -rotate-20">
-        <StatueOfLibertySvg className="w-30 h-auto" />
+      <div
+        className="absolute bottom-[20%] right-0 will-change-transform animate-float-4"
+        style={{ "--rotate": "-20deg" } as React.CSSProperties}
+      >
+        <FudaImage
+          src={StatueOfLibertyImage}
+          alt="Statue of Liberty"
+          className="w-[33%]"
+        />
       </div>
-      <div className="absolute bottom-60 right-10 transform rotate-10">
-        <PhotoFuda className="w-10 h-auto" />
+      <div
+        className="absolute bottom-[14%] right-[1.75%] will-change-transform animate-float-5"
+        style={{ "--rotate": "10deg" } as React.CSSProperties}
+      >
+        <FudaImage src={PhotoFudaImage} alt="Photo" className="w-[14%]" />
       </div>
     </div>
   );
--- src/app/(home)/_components/HeroSection/ArchDecoration/index.tsx
+++ src/app/(home)/_components/HeroSection/ArchDecoration/index.tsx
@@ -4,13 +4,16 @@ import ArchImage from "./soypoyArch.png";
 export default function ArchDecoration({ className }: { className: string }) {
   return (
     <div className={className}>
-      <Image
-        src={ArchImage}
-        alt="Arch Decoration"
-        width={1420}
-        height={426}
-        className="w-full h-auto"
-      />
+      {/* aspect-ratioを固定してレスポンシブ時の位置ずれを防ぐ */}
+      <div className="relative w-full aspect-[1420/426]">
+        <Image
+          src={ArchImage}
+          alt="Arch Decoration"
+          width={1420}
+          height={426}
+          className="w-full h-auto"
+        />
+      </div>
     </div>
   );
 }
