c11bc5a8845ffc3ec709e2a8cfa53f115ca481e3
syosyo10337 <syosyo.ds@gmail.com>
2025-10-21 02:44:05 +0900

    feat:微調整

--- src/app/(home)/_components/WhatUpSection/WhatUpButton.tsx
+++ src/app/(home)/_components/WhatUpSection/WhatUpButton.tsx
@@ -4,18 +4,21 @@ import type * as React from "react";
 import { cn } from "@/utils/cn";
 
 const buttonVariants = cva(
-  "relative overflow-hidden transition-all duration-300 ease-in-out group focus:outline-none focus:ring-2 focus:ring-soypoy-accent focus:ring-offset-2 bg-soypoy-main hover:bg-soypoy-accent border-soypoy-secondary border-2 hover:border-soypoy-main",
+  [
+    "relative overflow-hidden",
+    "transition-all duration-300 ease-in-out",
+    "group",
+    "focus:outline-none focus:ring-2 focus:ring-soypoy-accent focus:ring-offset-2",
+    "bg-soypoy-main hover:bg-soypoy-accent",
+    "border-soypoy-secondary border-2 hover:border-soypoy-main",
+  ],
   {
     variants: {
-      variant: {
-        default: "",
-      },
       size: {
-        default: "px-3 py-2 md:py-3 ",
+        default: "px-3 py-2 md:py-3",
       },
     },
     defaultVariants: {
-      variant: "default",
       size: "default",
     },
   },
@@ -32,7 +35,6 @@ interface WhatUpButtonProps
 // TODO: fontサイズがかなり分散している。フォントサイズを統一する。
 export default function WhatUpButton({
   className,
-  variant,
   size,
   title,
   prefix,
@@ -40,17 +42,17 @@ export default function WhatUpButton({
   ...props
 }: WhatUpButtonProps) {
   const textTransitionClasses = "transition-colors duration-300";
+  const firstChar = title[0];
+  const restChars = title.slice(1);
 
   return (
-    <button
-      className={cn(buttonVariants({ variant, size }), className)}
-      {...props}
-    >
-      <div className="flex items-center justify-between w-full">
-        <div className="flex items-center space-x-2 md:space-x-3">
+    <button className={cn(buttonVariants({ size }), className)} {...props}>
+      <div className="flex justify-between w-full">
+        <div className="flex gap-2 md:gap-3">
           <span
             className={cn(
-              "font-bernard-mt tracking-tight text-soypoy-accent group-hover:text-soypoy-main",
+              "font-bernard-mt tracking-tight",
+              "text-soypoy-accent group-hover:text-soypoy-main",
               "text-base md:text-[22px]",
               "pl-1 md:pl-2",
               "self-end pb-0.5 md:pb-1", // textを下に寄せるため
@@ -61,13 +63,16 @@ export default function WhatUpButton({
           </span>
           <div
             className={cn(
-              "font-anymale font-bold uppercase text-soypoy-secondary group-hover:text-soypoy-main",
+              "font-anymale font-bold uppercase",
+              "text-soypoy-secondary group-hover:text-soypoy-main",
               "text-3xl/7 md:text-[44px]/10",
               textTransitionClasses,
             )}
           >
-            <span className="text-[38px]/7 md:text-[58px]/10 ">{title[0]}</span>
-            {title.slice(1)}
+            <span className="text-[38px]/7 md:text-[58px]/10 ">
+              {firstChar}
+            </span>
+            {restChars}
           </div>
         </div>
         <div className="flex items-center px-2 md:px-3">
