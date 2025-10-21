f8fea975ad2612d94064de022f6ca02cc243d5b5
syosyo10337 <syosyo.ds@gmail.com>
2025-10-22 01:02:42 +0900

    微調整

--- src/app/(home)/_components/PIckUpSection/EventGrid/index.tsx
+++ src/app/(home)/_components/PIckUpSection/EventGrid/index.tsx
@@ -12,6 +12,8 @@ export default function EventGrid({ events }: EventGridProps) {
       className={cn(
         "grid grid-cols-2 xl:grid-cols-4",
         "w-full max-w-6xl mx-auto text-center",
+        "max-w-[73vw] md:max-w-[560px] xl:max-w-[80vw] 2xl:max-w-[70vw]", //NOTE: md~xlはタブレット表示なので横幅固定
+
       )}
     >
       {events.map((event, index) => (
