50bd4c549a0b395feefb21fa2d88b7905564d581
syosyo10337 <syosyo.ds@gmail.com>
2025-10-23 22:07:17 +0900

    bg fudaをシームレスに

--- src/app/(home)/_components/FudaFilmRollBg.tsx
+++ src/app/(home)/_components/FudaFilmRollBg.tsx
@@ -1,16 +1,16 @@
 // NOTE: Imageコンポーネントによる実装を検討したが、bg-imageとしての活用なので、現在の実装のままにする。
 export default function FudaFilmRollBg() {
   return (
-    <div className="film-roll-container">
-      <div className="film-roll-wrapper">
+    <div className="fixed top-0 right-0 bottom-0 left-0 w-full h-full pointer-events-none overflow-hidden">
+      <div className="flex justify-between items-stretch h-full">
         {/* 左側のフィルムロール */}
-        <div className="film-roll-side bg-filmroll-left film-roll-flow" />
+        <div className="flex shrink-0 w-8 md:w-14 bg-filmroll-left film-roll-flow" />
 
         {/* 中央の空白スペース */}
         <div className="flex-grow" />
 
         {/* 右側のフィルムロール */}
-        <div className="film-roll-side bg-filmroll-right film-roll-flow" />
+        <div className="flex shrink-0 w-8 md:w-14 bg-filmroll-right film-roll-flow" />
       </div>
     </div>
   );
