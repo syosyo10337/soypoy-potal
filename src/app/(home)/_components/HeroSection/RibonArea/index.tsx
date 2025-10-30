51621ad74e1a2aac0b59f9725f6840871e90e793
syosyo10337 <syosyo.ds@gmail.com>
2025-10-31 03:32:56 +0900

    feat:ribonアニメーションを追加

--- src/app/(home)/_components/HeroSection/RibonArea/index.tsx
+++ src/app/(home)/_components/HeroSection/RibonArea/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "motion/react";
 import { cn } from "@/utils/cn";
 import { Z_INDEX } from "../constants";
 import { HeroSecRibon } from "./assets";
@@ -5,7 +8,14 @@ import { InnerContent } from "./InnerContent";
 
 export default function RibonArea() {
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 5,
+        duration: 1,
+        ease: "easeOut",
+      }}
       className={cn(
         "max-w-4xl",
         "xl:max-w-5xl",
@@ -31,6 +41,6 @@ export default function RibonArea() {
           "lg:-mt-20",
         )}
       />
-    </div>
+    </motion.div>
   );
 }
