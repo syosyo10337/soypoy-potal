7e51e2a4a1d82c7afbfc80cd6b314bfaaaa36955
syosyo10337 <syosyo.ds@gmail.com>
2025-11-19 23:34:31 +0900

    feat:headerボタンのhoverカラー変更

--- src/components/Header/NavMenu.tsx
+++ src/components/Header/NavMenu.tsx
@@ -18,7 +18,7 @@ export function NavMenu({ navItems }: { navItems: NavItem[] }) {
         <Button
           variant="ghost"
           size="lg"
-          className="p-3 hover:bg-white/10 text-inherit font-display rounded-lg"
+          className="p-3 text-inherit font-display rounded-lg"
           aria-label="メニューを開く"
         >
           <Menu className="size-8" strokeWidth={1.2} />
