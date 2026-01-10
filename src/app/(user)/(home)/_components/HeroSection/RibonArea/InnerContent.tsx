bd1725453006c52a7c2266b672351d413b10b3c1
syosyo10337 <syosyo.ds@gmail.com>
2026-01-11 03:11:35 +0900

    feat:update RibonArea

--- src/app/(user)/(home)/_components/HeroSection/RibonArea/InnerContent.tsx
+++ src/app/(user)/(home)/_components/HeroSection/RibonArea/InnerContent.tsx
@@ -1,7 +1,6 @@
 import { Separator } from "@/components/shadcn/separator";
 import { cn } from "@/utils/cn";
 import { AboutLinkButton } from "./AboutLinkButton";
-import { MoonIcon, SunIcon } from "./assets";
 
 interface InnerContentProps {
   className?: string;
@@ -10,72 +9,19 @@ interface InnerContentProps {
 export function InnerContent({ className }: InnerContentProps) {
   return (
     <div className={cn("p-4", className)}>
-      <Title />
       <div
         className={cn(
           "flex",
           "flex-col lg:flex-row",
-          "gap-3 md:gap-0 lg:gap-4",
+          "gap-3 md:gap-0 lg:gap-4 lg:mt-14",
         )}
       >
-        <MainContent />
-        <StyledSeparator />
         <SoypoySlogan />
       </div>
       <div className={cn("flex justify-center", "mt-6 md:mt-8")}>
         <AboutLinkButton />
       </div>
     </div>
-  );
-}
-
-function Title() {
-  return (
-    <div className={cn("text-center", "pb-2 pt-2 md:pb-0")}>
-      <h2 className="text-xl font-bold font-anymale flex items-center justify-center">
-        <SunIcon className="w-10 h-10" />
-        <span className="text-2xl">About SOY-POY</span>
-        <MoonIcon className="w-10 h-10" />
-      </h2>
-    </div>
-  );
-}
-
-function MainContent() {
-  const text = `SOY-POYは「好きに生きて、一緒に生きる」をコンセプトに
-  オープンしたバーとステージのある週末限定のパブリックハウス
-  （PUB）です。下北沢に拠点を構えるSOY-POYは、ジャンル
-  を問わず、さまざまな表現や創作をする人たちが集い、互いに
-  交流しながら、誰もが自由に表現や創作活動を楽しめるコミュ
-  ニティづくりをおこなっています。`;
-
-  return (
-    <div className="flex-1 lg:w-7/10">
-      <p
-        className={cn("leading-relaxed", "text-sm/6 md:text-lg/8 font-medium")}
-      >
-        {text}
-      </p>
-    </div>
-  );
-}
-
-function StyledSeparator() {
-  return (
-    <>
-      <div className="hidden md:block lg:pb- lg:pt-2">
-        <Separator
-          orientation="vertical"
-          className="h-full !w-[2px] bg-soypoy-secondary"
-        />
-      </div>
-      <div className="block md:hidden">
-        <Separator
-          orientation="horizontal"
-          className="!h-[1px] bg-soypoy-secondary"
-        />
-      </div>
-    </>
   );
 }
 
@@ -96,46 +42,50 @@ function SoypoySlogan() {
   ];
 
   return (
-    <div className="lg:w-3/10 space-y-4">
-      <div className="space-y-3">
-        <div
-          className={cn("flex flex-col gap-2", "items-center lg:items-start")}
-        >
-          {slogans.map((slogan) => (
-            <div
-              className="flex items-start text-center lg:text-left"
-              key={slogan.title}
-            >
-              <div className="pb-1">
-                <h3
+    <div
+      className={cn(
+        "flex flex-col justify-between items-center",
+        "lg:flex-row lg:items-center lg:justify-center lg:w-full lg:gap-x-4 xl:gap-x-6",
+      )}
+    >
+      {slogans.map((slogan, index) => (
+        <div key={slogan.title} className="contents">
+          {index > 0 && (
+            <Separator
+              orientation="vertical"
+              className="hidden lg:block !w-[1px] bg-soypoy-secondary"
+            />
+          )}
+          <div className="flex items-center justify-center text-center">
+            <div className="pb-1">
+              <h3
+                className={cn(
+                  "font-bold font-anymale",
+                  "text-xl md:text-2xl lg:text-3xl",
+                )}
+              >
+                <span
                   className={cn(
-                    "font-bold font-anymale",
-                    "text-xl md:text-2xl",
+                    "text-2xl md:text-3xl lg:text-4xl",
+                    "font-black text-soypoy-accent",
                   )}
                 >
-                  <span
-                    className={cn(
-                      "text-2xl md:text-3xl ",
-                      "font-black text-soypoy-accent",
-                    )}
-                  >
-                    {slogan.title[0]}
-                  </span>
-                  {slogan.title.slice(1)}
-                </h3>
-                <p
-                  className={cn(
-                    "text-base md:text-lg",
-                    "font-zen-old-mincho leading-none",
-                  )}
-                >
-                  {slogan.description}
-                </p>
-              </div>
+                  {slogan.title[0]}
+                </span>
+                {slogan.title.slice(1)}
+              </h3>
+              <p
+                className={cn(
+                  "text-base md:text-lg lg:text-2xl",
+                  "font-zen-old-mincho leading-none",
+                )}
+              >
+                {slogan.description}
+              </p>
             </div>
-          ))}
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
